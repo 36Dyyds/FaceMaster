66576782d64a45db4d034622d0bd96019cec1f9e
花开富贵 <153128151@qq.com>
2024-04-01 16:50:39 +0800

    【新增】add_chinese_text_to_image.py（向图像添加中文文本）

---
+++ add_chinese_text_to_image.py
@@ -0,0 +1,82 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+
+def save_image(image, output_path):
+    """
+    保存图像到指定路径。
+
+    参数：
+    - image: 要保存的图像，OpenCV 格式的图像数组。
+    - output_path: 要保存的目标文件路径，包括文件名和文件扩展名。
+    """
+    # 确保输出文件夹存在
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # 尝试保存图像
+    success = cv2.imwrite(output_path, image)
+
+    # 检查保存是否成功
+    if success:
+        print('图像已保存到:', output_path)
+    else:
+        print('保存图像失败')
+
+
+def add_chinese_text(image, text, font_path='font/simsun.ttc', font_size=30, font_color=(0, 255, 0), position=(10, 10)):
+    """
+    在图像上添加中文文本。
+
+    参数：
+    - image: 要添加文本的图像，OpenCV 格式的图像数组。
+    - text: 要添加的文本内容。
+    - font_path: 中文字体文件的路径，默认为 'font/simsun.ttc'。
+    - font_size: 字体大小，默认为 30。
+    - font_color: 字体颜色，默认为绿色。
+    - position: 文本左上角的坐标，默认为 (10, 10)。
+    """
+    # 转换图像格式为 RGB（PIL 要求图像格式为 RGB）
+    pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # 创建绘图对象
+    draw = ImageDraw.Draw(pil_image)
+
+    # 加载中文字体
+    font = ImageFont.truetype(font_path, font_size)
+
+    # 在图像上绘制文本
+    draw.text(position, text, font=font, fill=font_color)
+
+    # 将 PIL 图像转换回 OpenCV 格式
+    return cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+
+
+def main():
+    image_path = 'images/001.jpg'
+    image = cv2.imread(image_path)
+
+    # 检查是否成功读取图片
+    if image is not None:
+        # 缩放图片
+        resized_image = cv2.resize(image, None, fx=0.5, fy=0.5)
+
+        # 在图像上添加中文文本
+        text = '你好，PIL！'
+        cv2_image_with_text = add_chinese_text(resized_image, text)
+
+        # 显示图片
+        cv2.imshow('Image with Chinese Text', cv2_image_with_text)
+        cv2.waitKey(0)  # 等待键盘输入，0 表示持续等待直到按下任意键
+        cv2.destroyAllWindows()  # 关闭所有窗口
+
+        # 将图像保存到本地
+        output_path = 'output/001.jpg'
+        save_image(cv2_image_with_text, output_path)
+    else:
+        print('无法加载图像：', image_path)
+
+
+if __name__ == "__main__":
+    main()
