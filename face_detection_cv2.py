981333043ba96122b85c16c295e3c08e3fcdbd57
花开富贵 <153128151@qq.com>
2024-04-07 01:17:33 +0800

    【新增】人脸识别 opencv 算法

---
+++ face_detection_cv2.py
@@ -0,0 +1,23 @@
+import cv2
+
+# 加载人脸检测器
+face_cascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+
+# 读取图片
+image_path = 'images/003.jpg'
+image = cv2.imread(image_path)
+
+# # 缩放图片
+# image = cv2.resize(image, None, fx=0.8, fy=0.8)
+
+# 检测人脸
+faces = face_cascade.detectMultiScale(image)
+
+# 在图像中标记人脸
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# 显示图像
+cv2.imshow('Emotion Detection', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
